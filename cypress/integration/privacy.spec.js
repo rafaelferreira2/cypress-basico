adac80cc3d1129423737d21cbfd0e2c32e943e68
rafaelferreira2 <sf.rafael20@gmail.com>
2022-07-22 08:49:19 -0300

    finishing basic cypress course

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,7 @@
-it.only('testa a página da política de privavidade de forma independente', function() {
-    cy.visit('./src/privacy.html')
-    cy.get('#title').should('have.text', 'CAC TAT - Política de privacidade')
-    cy.contains('Talking About Testing').should('be.visible')
+Cypress._.times(5, function(){
+    it('testa a página da política de privavidade de forma independente', function() {
+        cy.visit('./src/privacy.html')
+        cy.get('#title').should('have.text', 'CAC TAT - Política de privacidade')
+        cy.contains('Talking About Testing').should('be.visible')
+    })
 })
